a7fb3d5f5868cb99639ff146f612b927f752f136
ShyFen <995576918@qq.com>
2022-09-23 15:13:56 +0800

    菜单栏左侧图标设置

--- src/layout/components/Sidebar/Item.vue
+++ src/layout/components/Sidebar/Item.vue
@@ -1,41 +1,42 @@
 <script>
-export default {
-  name: 'MenuItem',
-  functional: true,
-  props: {
-    icon: {
-      type: String,
-      default: ''
+  export default {
+    name: "MenuItem",
+    //functional表示此组件为函数式组件，函数式组件：只会接收props属性，只是一个函数
+    functional: true, 
+    props: {
+      icon: {
+        type: String,
+        default: "",
+      },
+      title: {
+        type: String,
+        default: "",
+      },
     },
-    title: {
-      type: String,
-      default: ''
-    }
-  },
-  render(h, context) {
-    const { icon, title } = context.props
-    const vnodes = []
+    render(h, context) {
+      const { icon, title } = context.props;
+      const vnodes = [];
 
-    if (icon) {
-      if (icon.includes('el-icon')) {
-        vnodes.push(<i class={[icon, 'sub-el-icon']} />)
-      } else {
-        vnodes.push(<svg-icon icon-class={icon}/>)
+      if (icon) {
+        if (icon.includes("el-icon")) {
+          vnodes.push(<i class={[icon, "sub-el-icon"]} />);
+        } else {
+          vnodes.push(<svg-icon icon-class={icon} />);
+        }
       }
-    }
 
-    if (title) {
-      vnodes.push(<span slot='title'>{(title)}</span>)
-    }
-    return vnodes
-  }
-}
+      if (title) {
+        vnodes.push(<span slot="title">{title}</span>);
+      }
+      return vnodes;
+    },
+  };
 </script>
 
 <style scoped>
-.sub-el-icon {
-  color: currentColor;
-  width: 1em;
-  height: 1em;
-}
+  .sub-el-icon {
+    color: currentColor;
+    width: 1em;
+    height: 1em;
+  }
 </style>
