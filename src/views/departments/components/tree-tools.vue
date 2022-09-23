c321eecbeba8076841af162775c69cde73bff29f
ShyFen <995576918@qq.com>
2022-09-23 17:34:19 +0800

    抽取公共的树形代码

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-row
+    type="flex"
+    justify="space-between"
+    align="middle"
+    style="height: 40px; width: 100%"
+  >
+    <el-col>
+      <!-- 左边 -->
+      <span>{{ treeData.name }}</span>
+    </el-col>
+    <el-col :span="4">
+      <el-row type="flex" justify="end">
+        <el-col>{{ treeData.manager }}</el-col>
+        <!-- 下拉菜单 -->
+        <el-col>
+          <el-dropdown>
+            <span>操作<i class="el-icon-arrow-down"></i></span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>添加子部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">编辑部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">删除部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  export default {
+    props: {
+      treeData: {
+        type: Object,
+      },
+      isRoot: {
+        type: Boolean,
+        default: false, //默认不传值是false 也就是默认不是根节点
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .card {
+    padding: 30px 140px;
+    font-size: 14px;
+  }
+</style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -2,66 +2,23 @@
   <div>
     <!-- 组织架构 -->
     <el-card class="card">
-      <el-row
-        type="flex"
-        justify="space-between"
-        align="middle"
-        style="height: 40px , width: 100%"
-      >
-        <el-col>
-          <!-- 左边 -->
-          <span>ShyFen公司管理平台</span>
-        </el-col>
-        <el-col :span="4">
-          <el-row type="flex" justify="end">
-            <el-col>负责人</el-col>
-            <!-- 下拉菜单 -->
-            <el-col>
-              <el-dropdown>
-                <span>操作<i class="el-icon-arrow-down"></i></span>
-                <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>选项一</el-dropdown-item>
-                </el-dropdown-menu>
-              </el-dropdown>
-            </el-col>
-          </el-row>
-        </el-col>
-      </el-row>
+      <tree-tools :treeData="company" :isRoot="true"></tree-tools>
 
-      <el-tree :data="data" :props="defaultProps" :default-expand-all="true">
-        <el-row
-          slot-scope="{ data }"
-          type="flex"
-          justify="space-between"
-          align="middle"
-          style="height: 40px; width: 100%"
-        >
-          <el-col>
-            <!-- 左边 -->
-            <span>{{ data.name }}</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row type="flex" justify="end">
-              <el-col>{{ data.manager }}</el-col>
-              <!-- 下拉菜单 -->
-              <el-col>
-                <el-dropdown>
-                  <span>操作<i class="el-icon-arrow-down"></i></span>
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>选项一</el-dropdown-item>
-                    <el-dropdown-item>选项二</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-          </el-col>
-        </el-row>
+      <el-tree
+        style="width: 100%"
+        :data="data"
+        :props="defaultProps"
+        :default-expand-all="true"
+      >
+        <tree-tools slot-scope="{ data }" :treeData="data"></tree-tools>
       </el-tree>
     </el-card>
   </div>
 </template>
 
 <script>
+  import TreeTools from "./components/tree-tools.vue";
+
   export default {
     data() {
       return {
@@ -78,7 +35,14 @@
           label: "name",
           children: "children",
         },
+        company: {
+          name: "shyfen公司管理平台",
+          manager: "负责人",
+        },
       };
+    },
+    components: {
+      TreeTools,
     },
   };
 </script>
